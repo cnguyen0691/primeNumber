f780742607eccd8e52aaaf1f534824b9251aacad
cnguyen0691 <chaunguyen0691@gmail.com>
2019-04-20 10:20:38 -0400

    add condition

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,31 @@
-
 package com.company;
-
-import java.util.Scanner;
-
+import  java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
         Scanner prime = new Scanner(System.in);
-        System.out.print("Please input your number: ");
+        System.out.print("Please enter your number : ");
         int primes = prime.nextInt();
 
-        for (int i = 1; i <= primes; i++) {
-            if (i == 1) {
-                System.out.println(i + " is not a prime number");
-            }
-            else if (i ==2 || i==3 || i ==5){
 
-                System.out.println(i +" is a prime number");
-            }
-            else if (i % 2 == 0 || i % 3 == 0 || i%5==0) {
-                System.out.println(i + " is not a prime number");
-            } else {
+        for (int i = 1; i <= primes; i++) {
+            if (primeNumber(i)) {
                 System.out.println(i + " is a prime number");
+            } else {
+                System.out.println(i + " is a not prime number");
             }
         }
     }
+
+    public static boolean primeNumber(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
